d6558fa62d4bd230656dfd97296f6ef4bbd9d551
kendraliu <120770996+kendraliu@users.noreply.github.com>
2023-08-20 22:24:58 -0700

    simple guage

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,9 @@ let dropdownValues = []
 let subjectCase = []
 let demoinfo = []
 
+d3.select("#selDataset").on("change", optionChanged)
+d3.select("#sample-metadata").on("change", optionChanged)
+
 jsondata.then(function(x)
 {
     console.log(x);
@@ -49,11 +52,25 @@ function optionChanged(individual){
             item.textContent = `${key}: ${demoinfo[index][key]}`
             demographicInfo.appendChild(item);
         }
-            
-            
-        
-    
 
+        let colors = ["#efeeb4", "#dad873", "#58b368", "#309975", "#454d66"]
+
+        let guageSettings = [{
+                domain: { x: [0, 1], y: [0, 1] },
+                value: demoinfo[index].wfreq,
+                title: { text: "belly button washing frequency" },
+                type: "indicator",
+                mode: "gauge+number+delta",
+                gauge: {
+                    axis: { range: [0, 5] },
+                    steps: Array.from({ length: 5 }, (_, i) => ({
+                        range: [i, i + 1],
+                        color: colors[i]
+                    })),
+                    },
+            }
+        ];
+        
         let top10OTUsGraphTrace = {
             x: values,
             y: ids.map(id => `OTU ${id}`),
@@ -84,80 +101,9 @@ function optionChanged(individual){
         
         Plotly.newPlot("individualData", top10OTUsGraph, layout)
         Plotly.newPlot("bubbleData", bubbleGraph)
-
-
-        
+        Plotly.newPlot('guage', guageSettings)
     }
 }
-
-
-d3.select("#selDataset").on("change", optionChanged)
-d3.select("#sample-metadata").on("change", optionChanged)
-
-
-// function optionChanged(y){
-//     console.log(y)
-
-// }
-// let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-
-// function grabdata(){
-//     //n = "941"
-//     d3.json(url).then(function(data) {
-//         console.log(data);
-//         //console.log(data.samples);
-//         console.log(data.samples[1].otu_ids)
-//         // if (data.names === data.samples.id){
-//         //     console.log(data.samples.id)
-//         // }
-         
-//       });
-// }
-
-// grabdata()
-
-// 
-// data = 
-
-
-    
-
-    // d3.select("#selDataset").on("change", function optionChanged(individual) {
-    //     console.log(individual)
-    //     for (let i=0; i < x.samples.length; i++){ // i = each individual
-    //         //console.log(x.samples[i])
-    //         if (individual === x.samples[i].otu_ids){
-    //             let labels = x.samples[i].otu_labels.slice(0, 10);
-    //             let ids = x.samples[i].otu_ids.slice(0, 10);
-    //             let values = x.samples[i].sample_values.slice(0, 10);
-    //             //console.log(values)
-
-        
-
-    //            let top10OTUsGraphTrace = {
-    //                 x: values,
-    //                 y: ids.map(id => `OTU ${id}`),
-    //                 text: labels,
-    //                 type: "bar",
-    //                 orientation: "h"
-    //            }
-
-    //            let top10OTUsGraph = [top10OTUsGraphTrace]
-
-    //             let layout = {
-    //                width: 800, 
-    //                 height: 400, 
-    //                 //
-    //             };
-
-    //             Plotly.newPlot("individualData", top10OTUsGraph, layout)}
-    //     }
-    // })
-
-
-
-
-
 
 
 
