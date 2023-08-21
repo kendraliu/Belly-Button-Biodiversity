d0ba0b3411b429af6b4a8db1d06c195a46133e64
kendraliu <120770996+kendraliu@users.noreply.github.com>
2023-08-20 21:19:41 -0700

    added demographic data

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,7 @@ jsondata = d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-cla
 
 let dropdownValues = []
 let subjectCase = []
+let demoinfo = []
 
 jsondata.then(function(x)
 {
@@ -9,6 +10,7 @@ jsondata.then(function(x)
     for (let i=0; i < x.samples.length; i++){
         dropdownValues.push(x.names[i])
         subjectCase.push(x.samples[i])
+        demoinfo.push(x.metadata[i])
         //console.log(x.names[i])
     }
 
@@ -19,29 +21,37 @@ jsondata.then(function(x)
         option.textContent = value;
         dropdownMenu.appendChild(option);
     }
-
-    //return x
-    //let biodivdata = getdata(x => x);
-    //let biodivdata
-    
-    /*console.log(x.names.length);
-    console.log(x.metadata.length);
-    console.log(x.samples.length);
-    console.log(x.samples[0]);*/
-
 });
+
+console.log(demoinfo)
 
 function optionChanged(individual){
     if (dropdownValues.includes(individual)) {
-        console.log(individual);
+        //console.log(individual);
         index = dropdownValues.indexOf(individual)
-        console.log(subjectCase[index].otu_ids)
+        //console.log(subjectCase[index].otu_ids)
         
         
         let labels = subjectCase[index].otu_labels.slice(0, 10);
         let ids = subjectCase[index].otu_ids.slice(0, 10);
         let values = subjectCase[index].sample_values.slice(0, 10);
-        console.log(values)
+        //console.log(values)
+
+        let demographicInfo = document.getElementById("sample-metadata");
+        
+            
+            console.log(demoinfo[index])
+            for (let key in demoinfo[index]){
+                // console.log(key)
+                // console.log(meta[key])
+                let item = document.createElement("p");
+                console.log(`${key}: ${demoinfo[index][key]}`)
+                item.textContent = `${key}: ${demoinfo[index][key]}`
+                demographicInfo.appendChild(item);
+            }
+            
+            
+        
     
 
         let top10OTUsGraphTrace = {
@@ -73,45 +83,16 @@ function optionChanged(individual){
         }
         
         Plotly.newPlot("individualData", top10OTUsGraph, layout)
-        Plotly.newPlot("bubbleData", bubbleGraph)}
+        Plotly.newPlot("bubbleData", bubbleGraph)
+
+
         
     }
+}
 
 
-d3.select("#selDataset").on("change", optionChanged
-    //console.log(individual)
-    
-    //     for (i in dropdownValues){
-    //     console.log(x.samples[i].otu_ids)
-
-        // if (individual === x.samples[i].id){
-        //     let labels = x.samples[i].otu_labels.slice(0, 10);
-        //     let ids = x.samples[i].otu_ids.slice(0, 10);
-        //     let values = x.samples[i].sample_values.slice(0, 10);
-        //     console.log(values)
-
-    
-
-        //    let top10OTUsGraphTrace = {
-        //         x: values,
-        //         y: ids.map(id => `OTU ${id}`),
-        //         text: labels,
-        //         type: "bar",
-        //         orientation: "h"
-        //    }
-
-        //    let top10OTUsGraph = [top10OTUsGraphTrace]
-
-        //     let layout = {
-        //        width: 800, 
-        //         height: 400, 
-        //         //
-        //     };
-
-    //     //     Plotly.newPlot("individualData", top10OTUsGraph, layout)}
-    // }
-
-     )
+d3.select("#selDataset").on("change", optionChanged)
+d3.select("#sample-metadata").on("change", optionChanged)
 
 
 // function optionChanged(y){
